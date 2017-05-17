bac4ea41f7fb20aefcab48d32cffb9e241ba6e52
Joshua Yeo <Yib@Yib-2.local>
2017-05-17 21:14:58 +0800

    updated looooong.py

--- Level 1/looooong.py
+++ Level 1/looooong.py
@@ -5,7 +5,7 @@ import re
 print("Enter the port number to connect to:")
 portnumber = input()
 if(portnumber<1024 or portnumber>65535):
-	exit()
+        exit()
 
 x = socket.socket()
 x.connect(("shell2017.picoctf.com", portnumber))
@@ -14,11 +14,11 @@ x.connect(("shell2017.picoctf.com", portnumber))
 a = re.findall(r"'(.*?)'", x.recv(4096))
 
 #Extracting the letter + number + ending
-b = a[0]
-c = int(a[1])
-d = a[2]
+b = a[0] 			#letter
+c = int(a[1])		#letter quantity
+d = a[2]			#ending value
 
-#constructing payload
+#constructing the payload
 payload = b * c
 payload += d
 payload += "\n"
@@ -26,3 +26,6 @@ payload += "\n"
 x.send(payload)
 sys.stdout.flush()
 print(x.recv(4096))
+
+#Source:https://github.com/Idomin/CTF-Writeups/blob/master/PicoCTF-2017/
+#           looooong
